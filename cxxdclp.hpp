6d77047d2ab329b5f7b3507c5a3c16cd0e505d64
Corentin <corentin2kr@gmail.com>
2020-03-04 22:58:46 -0400

    sources setup

---
+++ cxxdclp.hpp
@@ -0,0 +1,11 @@
+#ifndef CXXDCLP__HPP
+#define CXXDCLP__HPP
+
+#include "externs/tokenizer/tokenizer.hpp"
+
+void parse(const char* _path)
+{
+    tokenizer tok;
+}
+
+#endif // !CXXDCLP__HPP
---
+++ test.cpp
@@ -0,0 +1,7 @@
+
+#include "cxxdclp.hpp"
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
