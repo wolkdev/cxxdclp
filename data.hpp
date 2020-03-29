357bc1b11ef19cff71ea56757b77da6eae78875a
Corentin <corentin2kr@gmail.com>
2020-03-29 14:58:58 -0400

    data header

---
+++ data.hpp
@@ -0,0 +1,53 @@
+#ifndef DATA__HPP
+#define DATA__HPP
+
+#include <string>
+#include <vector>
+
+enum PROTECTION
+{
+    PUBLIC,
+    PRIVATE,
+    PROTECTED
+};
+
+struct common
+{
+    std::string name;
+    std::vector<std::string> type;
+
+    PROTECTION protection;
+    
+    bool isStatic;
+};
+
+struct variable : common
+{
+    std::vector<std::string> value;
+};
+
+struct function : common
+{
+    std::vector<variable> args;
+
+    bool isPure;
+    bool isVirtual;
+};
+
+struct type
+{
+    std::string name;
+    std::vector<std::string> fullName;
+    
+    PROTECTION protection;
+
+    bool isStruct;
+    bool isTemplate;
+
+    std::vector<std::string> templates;
+
+    std::vector<variable> variables;
+    std::vector<function> functions;
+};
+
+#endif // !DATA__HPP
